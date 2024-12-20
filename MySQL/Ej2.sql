a95dae963ac9bc84433115fee9948789afc7f11c
Ezequiel Reale <ezequiel.reale192436@alumnos.info.unlp.edu.ar>
2024-12-19 21:43:41 -0300

    Delete MySQL directory

--- MySQL/Ej2.sql
+++
@@ -1,9 +0,0 @@
--- 2) Hallar aquellos pacientes que para todas sus consultas médicas siempre
--- hayan dejado su número de teléfono primario (nunca el teléfono secundario).
-SELECT *
-FROM patient p
-WHERE p.secondary_phone NOT IN (
-    SELECT a.contact_phone
-    FROM appointment a
-    WHERE p.patient_id = a.patient_id
-    );
--- MySQL/Ej3.sql
+++
@@ -1,6 +0,0 @@
--- 3) Crear una vista llamada ‘doctors_per_patients’ que muestre los id de los
--- pacientes y los id de doctores de la ciudad donde vive el cliente.
-CREATE VIEW doctors_per_patients AS (
-    SELECT p.patient_id, d.doctor_id
-    FROM patient p INNER JOIN doctor d ON p.patient_city = d.doctor_city
-    );
